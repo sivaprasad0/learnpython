ba3f60bb35f4df31903b3798521871358cbc4769
durga <181873@ust-global.com>
2021-08-10 02:40:27 +0530

    both files add and mul

---
+++ adddsp.py
@@ -0,0 +1,4 @@
+x,y = [int(x) for x in input("enter 2 numbers").split()]
+
+print("the sum of the numbers",x+y)
+
--- muldsp.py
+++ muldsp.py
@@ -1,5 +1,6 @@
-x,y = input ("enter 2 numbers")
+x,y = input ("enter 2 numbers :").split()
+#x,y = [ int(x) for x in input("enter  2 numbers").split() ]
 x=int(x)
 y=int(y)
 
-print("the multiplication is : " + x*y)
+print("the multiplication is : ",x*y)
